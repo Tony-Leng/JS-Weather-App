b714b9ddc59b25b8b6770508c7aa08ff671f08ba
Tony Leng <lengtk@gmail.com>
2023-04-05 16:19:23 +0900

    added api url and key to js file

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 console.log('connected!');
 
-const location = document.querySelector('#location');
+const apiKey = "1535a41b848c877b2ff26535fb337d63";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=tokyo";
+
+async function checkWeather() {
+  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+  var data = await response.json();
+
+  console.log(data);
+}
+
+checkWeather();
