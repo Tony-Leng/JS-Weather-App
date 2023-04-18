4fbf266bd2b8ba3fb245cdfa3ce68b55f0b5abae
Tony Leng <lengtk@gmail.com>
2023-04-18 21:02:33 +0900

    added styling

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ async function checkWeather(city) {
   var data = await response.json();
 
   document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°Celcius";
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 }
